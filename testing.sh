e223cbea07e134d06d26f1e5afa6658c19bc8de2
JohnDoemnika <corppbuzsa@proton.me>
2026-08-28 12:29:25 +0000

    Add files via upload

---
+++ testing.sh
@@ -0,0 +1,27 @@
+echo "Niri Auto installer BETA V.0"
+echo "____________________________"
+read -p "Do you want to start the script ? [y/n] : " start
+if [ "$start" = "y" ]; then
+    echo "Installing niri, noctalia and git..."
+    sudo pacman -S noctalia niri git alacritty
+    echo "Cloning the config..."
+    git clone https://github.com/JohnDoemnika/niri-config.git
+    "copying the config..."
+    cp /home/$USER/niri-config ~/.config/niri
+    echo "the installation is done.
+    ___\___\___\___\___\___\_"
+    read -p "Do you want to reboot your computer ? [y/n]" rebootchc
+    if [ "$rebootchc" = "y" ]; then
+    echo "rebooting.  3"
+    sleep 1
+    echo "rebooting.. 2"
+    sleep 1
+    echo "rebooting...1"
+    sleep 1
+    sudo reboot
+    else
+    exit
+    fi
+else
+exit
+fi
