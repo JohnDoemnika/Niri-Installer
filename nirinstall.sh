63cbdae63bebbc185f977f67b4abd45834a7e53d
JohnDoemnika <corppbuzsa@proton.me>
2026-08-28 14:10:01 +0000

    Update nirinstall.sh

--- nirinstall.sh
+++ nirinstall.sh
@@ -8,7 +8,7 @@ if [ "$start" = "y" ]; then
     echo "enabling sddm..."
     sudo systemctl enable sddm
     echo "the installation is done.
-    ___\___\___\___\___\___\_"
+    \__\___\___\___\___\___\_"
     read -p "Do you want to reboot your computer ? [y/n]" rebootchc
     if [ "$rebootchc" = "y" ]; then
     echo "rebooting.  3"
